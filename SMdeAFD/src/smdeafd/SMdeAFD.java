f83165bbd651d05caa475d048fb0532d232b5365
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-04-25 15:01:55 -0300

    add system data input

--- SMdeAFD/src/smdeafd/SMdeAFD.java
+++ SMdeAFD/src/smdeafd/SMdeAFD.java
@@ -1,21 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package smdeafd;
 
-/**
- *
- * @author djalm
- */
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class SMdeAFD {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String arquivo = "C:\\Users\\djalm\\Documents\\GitHub\\Simulador-e-minimizador-de-AFD\\AFD\\dados.txt";
+        BufferedReader buffer = new BufferedReader(new FileReader(arquivo));
+        String linha = buffer.readLine();
+        while (linha != null) {
+            System.out.println(linha);
+            linha = buffer.readLine();
+        }
     }
-    
+
 }
