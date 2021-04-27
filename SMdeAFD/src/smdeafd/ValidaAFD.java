c80bef26314c457fde4a3b31d5d3de61d2e9a531
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-04-27 11:06:50 -0300

    add comments in validation class

--- SMdeAFD/src/smdeafd/ValidaAFD.java
+++ SMdeAFD/src/smdeafd/ValidaAFD.java
@@ -1,5 +1,8 @@
 package smdeafd;
 
+// Validar AFD: Cada estado deve ter uma saída para cada simbolo do alfabeto.
+// Validar AFD: Strings invalidas devem terminar em um estado não final.
+// Validar AFD: Não possui transições com o vazio.
 public class ValidaAFD {
     
 }
