b6ded62a7ef918dcd52f8262b14324b742cb46d8
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-04-27 11:13:19 -0300

    add comments in minimization class

--- SMdeAFD/src/smdeafd/MinimizaAFD.java
+++ SMdeAFD/src/smdeafd/MinimizaAFD.java
@@ -1,5 +1,9 @@
 package smdeafd;
 
+// Minimiza AFD: Eliminar estados inalcançáveis a partir do estado inicial.
+// Minimiza AFD: Separar estados finais e não finais.
+// Minimiza AFD: Validar novos grupos formados.
+// Minimiza AFD: Criar um novo AFD.
 public class MinimizaAFD {
     
 }
