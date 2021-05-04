0f3e93b78db2e424f5f9b67f34161217c2c29860
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-05-04 13:03:52 -0300

    add valid system

--- SMdeAFD/src/smdeafd/ValidaAFD.java
+++ SMdeAFD/src/smdeafd/ValidaAFD.java
@@ -1,8 +1,17 @@
 package smdeafd;
 
 // Validar AFD: Cada estado deve ter uma saída para cada simbolo do alfabeto.
-// Validar AFD: Strings invalidas devem terminar em um estado não final.
-// Validar AFD: Não possui transições com o vazio.
 public class ValidaAFD {
-    public boolean validar(AFD afd){}
+
+    public boolean validar(AFD afd) {
+        String[][] transicoes = afd.getTransicoes();
+        for (int i = 0; i < transicoes[0].length; i++) {
+            for (int j = 0; j < transicoes.length; j++) {
+                if (transicoes[j][i] == null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
